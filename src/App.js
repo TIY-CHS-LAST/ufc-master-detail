45a289e6645cc7250bf352cadd42d3d088e6e70e
Calvin Webster <calweb@gmail.com>
2017-08-04 09:41:44 -0400

    add routing, cleanup default state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import store from './store';
 import FighterList from './components/FighterList'
-
+import { BrowserRouter, Route } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <Provider store={store} >
-        <FighterList />
+        <BrowserRouter>
+          <Route exact path='/' component={FighterList} />
+        </BrowserRouter>
       </Provider>
     );
   }
--- src/components/FighterList.js
+++ src/components/FighterList.js
@@ -3,16 +3,12 @@ import FighterCard from './FighterCard'
 import { connect } from 'react-redux'
 import { getFighters } from '../actions'
 
-const FighterList = function (props) {
-    console.log(props);
-    const getPeople = function() {
-      props.dispatch(getFighters())
-    }
+const FighterList = function ({ fighters }) {
+
     return (
       <section className="fighters">
-        <button onClick={e => getPeople()}>get people</button>
-        {props.fighters
-          ? props.fighters.map(fighter => <FighterCard key={fighter.id} fighter={fighter} />)
+        {fighters
+          ? fighters.map(fighter => <FighterCard key={fighter.id} fighter={fighter} />)
           : null }
       </section>
     )
